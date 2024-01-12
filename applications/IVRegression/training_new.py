Puting the multiplication by 1/2 after the inverse in dual closed-form solver to fix numerical errors

applications/IVRegression/training_new.py
@@ -123,7 +123,8 @@ class Trainer:
         train_data, validation_data = generate_train_dsprite(data_size=self.args.data_size,
                                                             rand_seed=self.args.seed,
                                                             val_size=self.args.val_size)
-        inner_data, outer_data = split_train_data(train_data, split_ratio=self.args.split_ratio, rand_seed=self.args.seed, device=device)
+        inner_data, outer_data = split_train_data(train_data, split_ratio=self.args.split_ratio, rand_seed=self.args.seed, device=device, dtype=self.dtype)
+        test_data = TestDataSetTorch.from_numpy(test_data, device=device, dtype=self.dtype)
 
         # Weird scaling of lambdas done in training
         lam_V = self.args.lam_V*inner_data[0].size()[0]
@@ -199,7 +200,7 @@ class Trainer:
                                 parameter_and_buffer_dicts=outer_NN_dic, 
                                 args=X, 
                                 strict=True))
-            loss = torch.norm((instrumental_feature - treatment_feature)) ** 2
+            loss = torch.norm((instrumental_feature - treatment_feature)) ** 2# + 0.07 * torch.norm(instrumental_feature) ** 2
             return loss
 
         # Inner objective function with regularization
@@ -239,6 +240,9 @@ class Trainer:
 
         if self.args.dual_solver['name']=='funcBO.solvers.ClosedFormSolver':
             self.args.dual_solver['reg'] = lam_V
+
+        if self.args.dual_solver['name']=='funcBO.solvers.IVClosedFormSolver':
+            self.args.dual_solver['reg'] = lam_V
         
         self.inner_loss = fi
         self.outer_loss = fo
@@ -272,12 +276,12 @@ class Trainer:
             inner_value = self.inner_solution(Z_outer)
             metrics_dict['forward_time']= time.time() - forward_start
             loss, u = self.outer_loss(inner_value, Y_outer)
+            wandb.log({"out. loss": loss.item()})
             # Backpropagation
             self.outer_optimizer.zero_grad()
             backward_start = time.time()
             loss.backward()
             self.outer_optimizer.step()
-            wandb.log({"out. loss": loss.item()})
             wandb.log({"outer var. norm": torch.norm(self.outer_param).item()})
             wandb.log({"outer var. grad. norm": torch.norm(self.outer_param.grad).item()})
             metrics_dict['norm_grad_outer_param']= torch.norm(self.outer_param.grad).item()

datasets/dsprite/dsprite_data_generator.py
@@ -33,15 +33,15 @@ class TrainDataSetTorch(NamedTuple):
     structural: torch.Tensor
 
     @classmethod
-    def from_numpy(cls, train_data: TrainDataSet, device='cpu'):
+    def from_numpy(cls, train_data: TrainDataSet, device='cpu', dtype=torch.float32):
         covariate = None
         if train_data.covariate is not None:
-            covariate = torch.tensor(train_data.covariate, dtype=torch.float32).to(device)
-        return TrainDataSetTorch(treatment=torch.tensor(train_data.treatment, dtype=torch.float32).to(device),
-                                 instrumental=torch.tensor(train_data.instrumental, dtype=torch.float32).to(device),
+            covariate = torch.tensor(train_data.covariate, dtype=dtype).to(device)
+        return TrainDataSetTorch(treatment=torch.tensor(train_data.treatment, dtype=dtype).to(device),
+                                 instrumental=torch.tensor(train_data.instrumental, dtype=dtype).to(device),
                                  covariate=covariate,
-                                 outcome=torch.tensor(train_data.outcome, dtype=torch.float32).to(device),
-                                 structural=torch.tensor(train_data.structural, dtype=torch.float32).to(device))
+                                 outcome=torch.tensor(train_data.outcome, dtype=dtype).to(device),
+                                 structural=torch.tensor(train_data.structural, dtype=dtype).to(device))
 
     def to_gpu(self):
         covariate = None
@@ -60,13 +60,13 @@ class TestDataSetTorch(NamedTuple):
     structural: torch.Tensor
 
     @classmethod
-    def from_numpy(cls, test_data: TestDataSet, device='cpu'):
+    def from_numpy(cls, test_data: TestDataSet, device='cpu', dtype=torch.float32):
         covariate = None
         if test_data.covariate is not None:
-            covariate = torch.tensor(test_data.covariate, dtype=torch.float32).to(device)
-        return TestDataSetTorch(treatment=torch.tensor(test_data.treatment, dtype=torch.float32).to(device),
+            covariate = torch.tensor(test_data.covariate, dtype=dtype).to(device)
+        return TestDataSetTorch(treatment=torch.tensor(test_data.treatment, dtype=dtype).to(device),
                                 covariate=covariate,
-                                structural=torch.tensor(test_data.structural, dtype=torch.float32).to(device))
+                                structural=torch.tensor(test_data.structural, dtype=dtype).to(device))
     def to_gpu(self):
         covariate = None
         if self.covariate is not None:
@@ -117,9 +117,6 @@ def generate_test_dsprite(device):
     treatment = imgs[image_idx_arr].reshape((data_size, 64 * 64))
     structural = structural_func(treatment, weights)
     structural = structural[:, np.newaxis]
-    # Moving to gpu
-    treatment = torch.from_numpy(treatment).float().to(device)
-    structural = torch.from_numpy(structural).float().to(device)
     return TestDataSet(treatment=treatment, covariate=None, structural=structural)
 
 def generate_train_dsprite(data_size, rand_seed, val_size=0):
@@ -171,7 +168,7 @@ def generate_train_dsprite(data_size, rand_seed, val_size=0):
     return train_data_final, validation_data_final
 
 
-def split_train_data(train_data, split_ratio, rand_seed=42, device='cpu'):
+def split_train_data(train_data, split_ratio, rand_seed=42, device='cpu', dtype=torch.float32):
     n_data = train_data[0].shape[0]
     idx_train_1st, idx_train_2nd = train_test_split(np.arange(n_data), train_size=split_ratio, random_state=rand_seed)
 

datasets/dsprite/trainer.py
@@ -175,9 +175,9 @@ class DFIVTrainer:
             loss = res["stage2_loss"]
             wandb.log({"out. loss": loss.item()})
             loss.backward()
+            self.treatment_opt.step()
             wandb.log({"outer var. norm": (torch.sqrt(sum([torch.norm(p.data)**2 for p in self.treatment_net.parameters()]))).item()})
             wandb.log({"outer var. grad. norm": (torch.sqrt(sum([torch.norm(p.grad)**2 for p in self.treatment_net.parameters()]))).item()})
-            self.treatment_opt.step()
         return res["stage2_weight"]
     
     def evaluate(self, test_dataset, u):

funcBO/InnerSolution_new.py
@@ -85,8 +85,8 @@ class InnerSolution(nn.Module):
     self.dual_solver = config_to_instance(**dual_solver_args,
                                       model = self.dual_model,
                                       objective=self.dual_objective)
-    if isinstance(self.dual_solver,ClosedFormSolver):
-      assert isinstance(self.dual_model,LinearDualNetwork)
+    if isinstance(self.dual_solver, ClosedFormSolver):
+      assert isinstance(self.dual_model, LinearDualNetwork)
 
   def register_outer_parameters(self,outer_model):
     if isinstance(outer_model,nn.Module):

funcBO/objectives.py
@@ -125,8 +125,23 @@ class DualObjective:
     hessian = torch.einsum('bij, bc, bd->icjd',hessian,inner_features,inner_features)
     outer_grad = outer_grad.flatten(start_dim=1)
     B = torch.einsum('bi, bc->ic',outer_grad,outer_features)
-
-    #### TODO: some reshaping into matrix and vector
-
     return hessian, B
 
+  def make_reduced_linear_system(self, dual_model,
+                                outer_param,
+                                dual_model_inputs, 
+                                outer_grad):
+      data = self.objective.get_data(use_previous_data=True)
+      inner_model_inputs, inner_loss_inputs =  self.objective.data_projector(data)
+      inner_model = self.inner_model
+      inner_model.eval()
+      with torch.no_grad():
+        inner_model_output = inner_model(inner_model_inputs)
+        _,inner_features = dual_model(inner_model_inputs,with_features=True)
+        _,outer_features = dual_model(dual_model_inputs,with_features=True)
+
+      output_shape = inner_model_output.shape[1:]
+      hessian = torch.einsum('bc, bd->cd',inner_features,inner_features)
+      outer_grad = outer_grad.flatten(start_dim=1)
+      B = torch.einsum('bi, bc->ic',outer_grad,outer_features)
+      return hessian, B

funcBO/solvers.py
@@ -70,7 +70,7 @@ class ClosedFormSolver(Solver):
   """
   Solver that finds a*( ) in closed form.
   """
-  def __init__(self,model,objective,reg=0.):
+  def __init__(self, model, objective, reg=0.):
     super(ClosedFormSolver, self).__init__(model, objective)
     self.reg = reg
   def run(self,*objective_args):
@@ -84,5 +84,21 @@ class ClosedFormSolver(Solver):
     hessian += self.reg*torch.eye(hessian.shape[0], dtype= hessian.dtype, device= hessian.device)
     W = -torch.linalg.solve(hessian, B)
     W = torch.unflatten(W, dim=0, sizes=weight_shape)
+    weights = list(self.model.parameters())
+    weights[0].data = W.detach()
+
+class IVClosedFormSolver(Solver):
+  """
+  Solver that finds a*( ) in closed form.
+  """
+  def __init__(self, model, objective, reg=0.):
+    super(IVClosedFormSolver, self).__init__(model, objective)
+    self.reg = reg
+  def run(self,*objective_args):
+    hessian, B = self.objective.make_reduced_linear_system(self.model, *objective_args)
+    weight_shape = B.shape
+    hessian += self.reg*torch.eye(hessian.shape[0], dtype= hessian.dtype, device= hessian.device)
+    H_in = torch.inverse(hessian)
+    W = -0.5*torch.einsum('dc, ic->id', H_in, B)
     weights = list(self.model.parameters())
     weights[0].data = W.detach()

tests/DFIV.py
@@ -19,6 +19,7 @@ else:
     print("No GPUs found, setting the device to CPU.")
 
 # Setting hyper-parameters
+dtype = torch.float32
 seed = 42
 max_epochs = 5000
 max_inner_iters = 20
@@ -29,8 +30,8 @@ lam1 = 0.1
 test_data = generate_test_dsprite(device=device)
 train_data, validation_data = generate_train_dsprite(data_size=5000,
                                                     rand_seed=seed)
-inner_data, outer_data = split_train_data(train_data, split_ratio=0.5, rand_seed=seed, device=device)
-test_data = TestDataSetTorch.from_numpy(test_data, device=device)
+inner_data, outer_data = split_train_data(train_data, split_ratio=0.5, rand_seed=seed, device=device, dtype=dtype)
+test_data = TestDataSetTorch.from_numpy(test_data, device=device, dtype=dtype)
 
 # Neural networks for dsprites data
 inner_model, outer_model = build_net_for_dsprite(seed, method='sequential')