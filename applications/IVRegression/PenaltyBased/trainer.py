601b307ff5ef55b351490ac0da349729d152f2ac
Ieva Petrulionyte <ievapetrulionyte@gmail.com>
2024-08-05 19:12:53 +0200

    penalty based

---
+++ applications/IVRegression/PenaltyBased/trainer.py
@@ -0,0 +1,128 @@
+import torch
+from applications.IVRegression.dataset_networks_dsprites.twoSLS import *
+from applications.IVRegression.dataset_networks_dsprites.generator import *
+import time
+from funcBO.utils import assign_device, get_dtype
+
+class Trainer:
+    """
+    Solves an instrumental regression problem using the Gradient Penalty method.
+    """
+    def __init__(self, config, logger):
+        """
+        Initializes the Trainer class with the provided configuration and logger.
+
+        Parameters:
+        - config (object): Configuration object containing various settings.
+        - logger (object): Logger object for logging metrics and information.
+        """
+        self.logger = logger
+        self.args = config
+        self.device = assign_device(self.args.system.device)
+        self.dtype = get_dtype(self.args.system.dtype)
+        torch.set_default_dtype(self.dtype)
+        self.build_trainer()
+
+    def log(self, dico, log_name='metrics'):
+        self.logger.log_metrics(dico, log_name=log_name)
+
+    def build_trainer(self):
+        """
+        Builds the trainer by setting up data, models, and optimization components (number of epochs, optimizer, etc.).
+        """
+        # Generate synthetic dsprites data
+        self.test_data = generate_test_dsprite(device=self.device)
+        self.train_data, validation_data = generate_train_dsprite(data_size=self.args.data_size,
+                                                            rand_seed=self.args.seed,
+                                                            val_size=self.args.val_size)
+        if validation_data:
+            self.validation_data = TrainDataSetTorch.from_numpy(validation_data, device=self.device, dtype=self.dtype)
+        else:
+            self.validation_data = validation_data
+        self.inner_data, self.outer_data = split_train_data(self.train_data, split_ratio=self.args.split_ratio, rand_seed=self.args.seed, device=self.device, dtype=self.dtype)
+        self.test_data = TestDataSetTorch.from_numpy(self.test_data, device=self.device, dtype=self.dtype)
+
+        self.instrumental_network, self.treatment_network = build_net_for_dsprite(self.args.seed, method='sequential')
+        self.instrumental_network.to(self.device)
+        self.treatment_network.to(self.device)
+        # Optimizer that improves the treatment network
+        params = list(self.treatment_network.parameters()) + list(self.instrumental_network.parameters())
+        self.optimizer = torch.optim.Adam(params,
+                                        lr=self.args.optimizer.lr, 
+                                        weight_decay=self.args.optimizer.wd)
+        # Hyperparameters
+        self.penalty = self.args.penalty
+        self.lam1 = self.args.lam_V
+        self.lam2 = self.args.lam_u
+        
+    def train(self):
+        """
+        The main optimization loop for the Penalty method.
+        """
+        self.lam1 *= self.inner_data[0].size()[0]
+        self.lam2 *= self.outer_data[0].size()[0]
+        for epoch in range(self.args.max_epochs):
+            self.optimizer.zero_grad()
+            # Compute the loss
+            loss = self.penalty_loss()
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+            # Log the metrics
+            self.log({'iter': epoch, 'loss': loss.item()}, log_name='metrics')
+        # Cmopute the validation loss
+        if self.validation_data:
+            self.log({'iter': epoch, 'val_loss': (self.evaluate(self.inner_data, self.outer_data, data_type='validation_data')).item()}, log_name='val_metrics')
+        # Evaluate the model
+        self.log({'iter': epoch, 'test loss': (self.evaluate(self.inner_data, self.outer_data)).item()}, log_name='test_metrics')
+
+    def penalty_loss(self):
+        # Get the value of f(X)
+        treatment_feature = self.treatment_network(self.inner_data.treatment).detach()
+        # Get the value of g(Z)
+        instrumental_feature = self.instrumental_network(self.inner_data.instrumental)
+        feature = augment_stage1_feature(instrumental_feature)
+        loss1 = linear_reg_loss(treatment_feature, feature, self.lam1)
+        # Compute the L2 gradient norm of the stage 1 loss
+        loss1.backward()
+        grad_norm = torch.sqrt(sum([torch.norm(p.grad)**2 for p in self.instrumental_network.parameters()]))
+        # Compute stage 2 loss
+        # Get the value of g(Z)_stage1
+        instrumental_1st_feature = self.instrumental_network(self.inner_data.instrumental).detach()
+        # Get the value of g(Z)_stage2
+        instrumental_2nd_feature = self.instrumental_network(self.outer_data.instrumental).detach()
+        # Get the value of f(X)_stage1
+        treatment_1st_feature = self.treatment_network(self.inner_data.treatment)
+        res = fit_2sls(treatment_1st_feature, instrumental_1st_feature, instrumental_2nd_feature, self.outer_data.outcome, self.lam1, self.lam2)
+        loss2 = res["stage2_loss"]
+        return loss2 + self.penalty*grad_norm
+    
+    def evaluate(self, stage1_dataset, stage2_dataset, data_type='test_data'):
+        """
+        Evaluate the prediction quality on the test dataset.
+        """
+        self.treatment_network.eval()
+        self.instrumental_network.eval()
+        with torch.no_grad():
+            # Find stage2_weight
+            treatment_1st_feature = self.treatment_network(stage1_dataset.treatment).detach()
+            instrumental_1st_feature = self.instrumental_network(stage1_dataset.instrumental).detach()
+            instrumental_2nd_feature = self.instrumental_network(stage2_dataset.instrumental).detach()
+            feature = augment_stage1_feature(instrumental_1st_feature)
+            stage1_weight = fit_linear(treatment_1st_feature, feature, self.lam1)
+            feature = augment_stage1_feature(instrumental_2nd_feature)
+            predicted_treatment_feature = linear_reg_pred(feature, stage1_weight)
+            feature = augment_stage2_feature(predicted_treatment_feature)
+            stage2_weight = fit_linear(stage2_dataset.outcome, feature, self.lam2)
+            # Compute the test loss
+            if data_type == 'test_data':
+                Y_test = self.test_data.structural
+                X_test = self.test_data.treatment
+            elif data_type == 'validation_data':
+                Y_test = self.validation_data.outcome
+                X_test = self.validation_data.treatment
+            treatment_feature = self.treatment_network(X_test).detach()
+            test_feature = augment_stage2_feature(treatment_feature)
+            test_pred = linear_reg_pred(test_feature, stage2_weight)
+            loss = (torch.norm((Y_test - test_pred)) ** 2) / Y_test.size(0)
+        return loss
---
+++ applications/IVRegression/launch_Penalty.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define parent directories for working and log directories
+parent_work_dir="../../data/.workdir"
+
+parent_log_dir="../../data/outputs/Penalty5k_grid_search"
+HYDRA_FULL_ERROR=1 OC_CAUSE=1 python applications/IVRegression/PenaltyBased/main.py \
+                seed=1,2,3,4\
+                data_size=5000\
+                max_epochs=2000\
+                val_size=200\
+                lam_u=0.1,0.01,0.001\
+                lam_V=0.1,0.01,0.001\
+                penalty=1,0.1,0.01,0.001\
+                optimizer.lr=0.01,0.001,0.0001\
+                optimizer.weight_decay=0.1,0.01,0.001\
+                +mlxp.use_scheduler=False\
+                +mlxp.use_version_manager=False\
+                +mlxp.interactive_mode=False\
+                +mlxp.use_logger=True\
+                +mlxp.logger.parent_log_dir=$parent_log_dir\
+                +mlxp.version_manager.parent_work_dir=$parent_work_dir\
